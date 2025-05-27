4d25164616975e973eb679f29d0038ba14e0cfac
Andreas Aronsson <BOLL@7708.net>
2025-05-27 22:41:28 +0200

    Adapt the web component
    Svelte seems nice, had to modify the tsconfig to fix my imports and Promise support... weird. Not sure if it was just a cache problem in WebStorm or something. Guess I'll leave it for now, as long as stuff "just works".

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,47 +1,42 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import actionbotLogo from '../public/actionbot_logo.svg'
+    import AppButton from './Components/AppButton.svelte'
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <div>
+        <a href="https://actionbot.app" target="_blank" rel="noreferrer">
+            <img src={actionbotLogo} class="logo" alt="ActionBot Logo"/>
+        </a>
+    </div>
+    <h1>ActionBot</h1>
+    <div>
+        <AppButton buttonText="Setup"></AppButton>
+        <AppButton buttonText="Editor"></AppButton>
+        <AppButton buttonText="Dashboard"></AppButton>
+        <AppButton buttonText="Presenter"></AppButton>
+    </div>
+    <div>
+        <p>The idea here is that these buttons will lead to individual SPAs, which all run Svelte.</p>
+        <p>This page should show a login form and then redirect to Setup if authentication is missing.</p>
+        <p>After auth and setup, every other component should be accessible.</p>
+    </div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    h1 {
+        font-family: "Tourney", sans-serif;
+        font-weight: normal;
+        margin-bottom: 1em;
+    }
+    .logo {
+        height: 16em;
+        padding: 1.5em;
+        will-change: filter;
+        transition: filter 300ms;
+    }
+
+    .logo:hover {
+        filter: drop-shadow(0 0 2em #fff);
+    }
 </style>
---
+++ web/src/Components/AppButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    let {buttonText = 'Button'} = $props();
+</script>
+<button>{buttonText}</button>
+<style>
+    button {
+        border: 4px double white;
+        background: transparent;
+        padding: 0 1em;
+        font-size: 2em;
+        color: white;
+        border-radius: 2em;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+        font-weight: normal;
+    }
+
+    button:hover {
+        background-color: #ffffff40;
+    }
+</style>
